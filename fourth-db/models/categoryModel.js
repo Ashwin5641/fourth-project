0944c7f2fa4597a4774071d23e4b0e6a16649254
Ashwin5641 <Ashwin5641>
2026-06-29 16:56:50 +0530

    created categories model

---
+++ fourth-db/models/categoryModel.js
@@ -0,0 +1,45 @@
+const db = require('../config/db');
+
+const categoryModel = {
+
+    getAllCategories: async () => {
+        const [rows] = await db.query(
+            'SELECT * FROM categories'
+        );
+        return rows;
+    },
+
+    getCategoryById: async (id) => {
+        const [rows] = await db.query(
+            'SELECT * FROM categories WHERE id = ?',
+            [id]
+        );
+        return rows[0]
+    },
+
+    createCategory: async (name, slug, parent_id, image, description) => {
+        const [result] = await db.query(
+            'INSERT INTO categories (name, slug, parent_id, image, description) VALUES (?, ?, ?, ?, ?)',
+            [name, slug, parent_id, image, description]
+        );
+        return result.insertId
+    },
+
+    updateCategory: async (id, name, slug, parent_id, image, description) => {
+        const [result] = await db.query(
+            'UPDATE categories SET name = ?, slug = ?, parent_id = ?, image = ?, description = ? WHERE id = ?',
+            [name, slug, parent_id, image, description, id]
+        );
+        return result.affectedRows;
+    },
+
+    deleteCategory: async (id) => {
+        const [result] = await db.query(
+            'DELETE FROM categories WHERE id = ?',
+            [id]
+        );
+        return result.affectedRows;
+    }
+}
+
+module.exports = categoryModel;
